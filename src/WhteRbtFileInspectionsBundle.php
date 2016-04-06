3de5bcd097680ec27850c7405e54155dd631c4ab
Marcel Kraus <mail@marcelkraus.de>
2016-04-07 00:09:24 +0200

    Create new bundle skeleton

---
+++ src/WhteRbtFileInspectionsBundle.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * This file is part of the WhteRbtFileInspectionsBundle.
+ *
+ * Copyright (c) 2016 Marcel Kraus <mail@marcelkraus.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace WhteRbt\FileInspectionsBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class WhteRbtFileInspectionsBundle extends Bundle
+{
+}
